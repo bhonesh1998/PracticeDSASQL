e23746bd9e9c608fc45fee74ba7be131ae0ea275
bhoneshchawla <bhonesh.mnnit@gmail.com>
2025-07-22 23:35:26 +0530

    22july2025

---
+++ DSA/Array/majority_element.py
@@ -0,0 +1,23 @@
+class Solution:
+    def majorityElement(self, a: List[int]) -> int:
+        c = 0
+        n = len(a)
+        ele = -1
+        for i in range(n):
+            if c == 0:
+                ele = a[i]
+                c += 1
+            elif ele == a[i]:
+                c += 1
+            else:
+                c -= 1
+        cn = 0
+        for i in range(n):
+            if a[i] == ele:
+                cn += 1
+        if cn >= (n / 2):
+            return ele
+
+
+
+
