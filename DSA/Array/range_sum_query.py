2dc1052f71f905f46c3188fc90585b8952fe54bd
bhonesh1998 <bhonesh.mnnit@gmail.com>
2026-02-05 13:44:03 +0530

    range_sum_query

--- DSA/Array/range_sum_query.py
+++ DSA/Array/range_sum_query.py
@@ -13,6 +13,7 @@ class NumArray:
         else:
             return self.pre[right] - self.pre[left - 1]
 
+
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
