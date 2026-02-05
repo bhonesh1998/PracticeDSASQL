a5c8ade4c042d37a1c8b3c10558193a666b4853c
bhonesh1998 <bhonesh.mnnit@gmail.com>
2026-02-05 13:38:06 +0530

    range_sum_query

---
+++ DSA/Array/range_sum_query.py
@@ -0,0 +1,18 @@
+from typing import List
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.pre = nums
+        for i in range(1, len(nums)):
+            self.pre[i] += self.pre[i - 1]
+
+    def sumRange(self, left: int, right: int) -> int:
+        if left == 0:
+            return self.pre[right]
+        else:
+            return self.pre[right] - self.pre[left - 1]
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
