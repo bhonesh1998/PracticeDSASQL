aec2a41e1a30dbb0bac22b77f72f6c57252baaa6
bhonesh1998 <bhonesh.mnnit@gmail.com>
2026-04-05 11:15:08 +0530

    https://leetcode.com/problems/assign-cookies/

---
+++ DSA/Greedy/assign_cookies.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/assign-cookies/
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        lg=len(g)
+        ls=len(s)
+        g.sort()
+        s.sort()
+        gp=0
+        sp=0
+        while sp<ls and gp<lg:
+            if g[gp]<=s[sp]:
+                gp+=1
+            sp+=1
+        return gp
